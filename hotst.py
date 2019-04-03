f4a5d5d994adf7a0a0ffb4ce2e9717be9ee84040
rniranjan93 <33223840+rniranjan93@users.noreply.github.com>
2019-04-03 23:12:35 +0530

    Add files via upload

---
+++ hotst.py
@@ -0,0 +1,28 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+j=input()
+i=int(input())
+i=i-1
+chrome_options=webdriver.ChromeOptions()
+chrome_options.add_argument("--disable-infobars")
+browser1 = webdriver.Chrome(r"C:\Users\Niranjan\Desktop\new\chromedriver.exe", chrome_options=chrome_options)
+browser1.set_window_position(1000, 1000)
+browser1.get(j)
+time.sleep(4)
+webdriver.ActionChains(browser1).send_keys("f").perform()
+while(1):
+    time.sleep(i*60+20)
+    browser = webdriver.Chrome(r"C:\Users\Niranjan\Desktop\new\chromedriver.exe", chrome_options=chrome_options)
+    browser.set_window_position(1000, 1000)
+    browser.get(j)
+    time.sleep(6)
+    webdriver.ActionChains(browser).send_keys("f").perform()
+    browser1.close()
+    time.sleep(i*60+20)
+    browser1 = webdriver.Chrome(r"C:\Users\Niranjan\Desktop\new\chromedriver.exe", chrome_options=chrome_options)
+    browser1.set_window_position(1000, 1000)
+    browser1.get(j)
+    time.sleep(6)
+    webdriver.ActionChains(browser1).send_keys("f").perform()
+    browser.close()
